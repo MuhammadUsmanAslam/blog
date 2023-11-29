817de2c386eb358f5ddf8f3e571495b22e362890
MuhammadUsmanAslam <musmanaslam520@gmail.com>
2023-11-29 22:55:44 +0500

    Add basic backend project_setup and configurations

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const morgan = require("morgan");
+
+// env config
+dotenv.config();
+
+// Rest Object
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+// Routes
+app.get('/', (req, res) => {
+    res.status(200).json({msg: "you're on home page"})
+})
+
+// listen
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on ${process.env.DEV_MODE} mode PORT: ${process.env.PORT}`)
+})
